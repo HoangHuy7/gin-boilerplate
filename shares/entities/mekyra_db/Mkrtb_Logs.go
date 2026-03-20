872884c0589ae3ebb99c73bebafde07b9b5e46eb
hoanghuy7 <hoanghuy200701@gmail.com>
2026-03-20 15:39:08 +0700

    commit code

--- shares/entities/mekyra_db/Mkrtb_Logs.go
+++ shares/entities/mekyra_db/Mkrtb_Logs.go
@@ -10,6 +10,7 @@ import (
 type Mkrtb_Logs struct {
 	Id        int64     `gorm:"column:id;type:bigserial;primaryKey;autoIncrement"`
 	Action    string    `gorm:"column:action;type:varchar(100)"`
+	Status    string    `gorm:"column:status;type:varchar(100)"`
 	Feature   string    `gorm:"column:feature;type:varchar(255)"`
 	OldData   []byte    `gorm:"column:old_data;type:jsonb"`
 	NewData   []byte    `gorm:"column:new_data;type:jsonb"`
